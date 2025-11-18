d2972effaf1f6ad4ecc4802c624e0a93a4792d1f
fullstackdeveloper918 <fullstackdeveloper918@gmail.com>
2025-11-18 17:42:34 +0530

    fix issue of envelopcard

--- src/components/common/EnvelopCard.tsx
+++ src/components/common/EnvelopCard.tsx
@@ -1,173 +1,88 @@
 "use client";
-import { fetchFromServer } from "@/app/actions/fetchFromServer";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
-import Cookies from "js-cookie";
 import Image from "next/image";
 
-// PageCover Component
-const PageCover = React.forwardRef((props: any, ref: any) => {
-  return (
-    <div className="cover" ref={ref} data-density="hard">
-      {props.children}
-    </div>
-  );
-});
+const PageCover = React.forwardRef((props: any, ref: any) => (
+  <div
+    ref={ref}
+    data-density="hard"
+    className={`cover ${props.className || ""}`}
+    {...props}
+  >
+    {props.children}
+  </div>
+));
 PageCover.displayName = "PageCover";
 
-// Page Component
-const Page = React.forwardRef((props: any, ref: any) => {
-  return (
-    <div className="page" ref={ref}>
-      <p>{props.children}</p>
-    </div>
-  );
-});
+const Page = React.forwardRef((props: any, ref: any) => (
+  <div className="page" ref={ref}>
+    {props.children}
+  </div>
+));
 Page.displayName = "Page";
 
 const EnvelopCard = ({ getdata }: any) => {
   const { id } = useParams();
   const [responseData, setResponseData] = useState<any>(null);
-  const [shareImageData, setShareImageData] = useState<any>(null);
-  const gettoken = Cookies.get("auth_token");
 
   useEffect(() => {
+    if (!id) return;
+
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/card/users-cards`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${gettoken}`,
-            },
-          }
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/card/edit-messages-by-unique-id/${id}`
         );
-
-        const data = await response.json();
-        setShareImageData(data); // Store response data in state
-      } catch (error) {
-        console.error("Error fetching data:", error);
+        const data = await res.json();
+        setResponseData(data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
       }
     };
 
     fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (id) {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/card/edit-messages-by-unique-id/${id}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-
-          const data = await response.json();
-          setResponseData(data); 
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-
-      fetchData();
-    }
   }, [id]);
-  if (
-    !responseData ||
-    !responseData?.data ||
-    !Array.isArray(responseData?.data[0]?.editor_messages)
-  ) {
+
+  // ❗ DO NOT RENDER HTMLFlipBook UNTIL DATA IS LOADED
+  if (!responseData) {
     return <div>Loading...</div>;
   }
 
-  const cardShareData = shareImageData?.listing?.find(
-    (item: any) => item?.message_unique_id === id
-  );
-
-  console.log("cardShareData", cardShareData);
+  const messages = responseData?.data?.[0]?.editor_messages || [];
 
   return (
     <>
-      <style>
-        {`
-          .album-web {
-            background: rgb(255, 251, 251);
-            text-align: center;
-          }
-
-          .page {
-            box-shadow: 0 1.5em 3em -1em rgb(70, 69, 69);
-            position: relative;
-          }
-
-          .cover {
-            background-color: rgb(251, 225, 139);
-            box-shadow: 0 1.5em 3em -1em rgb(70, 69, 69);
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-            width: 100%;
-            position: relative;
-          }
-
-          .btn,
-          .form-control {
-            padding: 0;
-            border: 0;
-            border-radius: 0;
-            color: inherit;
-            appearance: none;
-            font-size: 1em;
-            line-height: 1.2;
-            padding: 0.5em var(--padding-x);
-            border-width: 2px;
-            border-style: solid;
-          }
-
-          .btn {
-            background-color: aquamarine;
-            display: inline-flex;
-            justify-content: center;
-            align-items: center;
-            --padding-x: 1.2em;
-            border-color: transparent;
-          }
-
-          .form-control {
-            --padding-x: 0.5em;
-          }
-
-          input {
-            text-align: center;
-          }
-
-          .formContainer {
-            align-items: center;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-
-          .bottom-text {
-            position: absolute;
-            bottom: 20px;
-            font-size: 1em;
-            color: #333;
-            text-align: center;
-            width: 100%;
-          }
-        `}
-      </style>
+      <style>{`
+        .album-web {
+          background: rgb(255, 251, 251);
+          text-align: center;
+        }
+        .page {
+          box-shadow: 0 1.5em 3em -1em rgb(70, 69, 69);
+          position: relative;
+        }
+        .cover {
+          background-color: rgb(251, 225, 139);
+          box-shadow: 0 1.5em 3em -1em rgb(70, 69, 69);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
+          width: 100%;
+          position: relative;
+        }
+        .bottom-text {
+          position: absolute;
+          bottom: 20px;
+          font-size: 1em;
+          color: #333;
+          text-align: center;
+          width: 100%;
+        }
+      `}</style>
 
       <div className="mt-5 mb-3">
         <HTMLFlipBook
@@ -195,54 +110,63 @@ const EnvelopCard = ({ getdata }: any) => {
           showPageCorners={false}
           disableFlipByClick={false}
         >
+          {/* Cover page */}
           <PageCover>
             <img
-              style={{ height: "100%" }}
+              style={{ height: "100%", width: "100%", objectFit: "cover" }}
               src={`${process.env.NEXT_PUBLIC_API_URL}/${getdata?.data?.images?.[0]?.card_images?.[0]}`}
               alt="content"
             />
           </PageCover>
 
-          {responseData?.data?.[0]?.editor_messages?.map(
-            (item: any, index: any) => (
-              <Page
-                key={`${item.slideIndex}-${index}`}
-                number={item.slideIndex}
-              >
-                {item && (
-                  <div
-                    style={{
-                      position: "absolute",
-                      left: item.x,
-                      top: item.y,
-                    }}
-                  >
-                    {item.type === "text" && (
-                      <div dangerouslySetInnerHTML={{ __html: item.content }} />
-                    )}
-                    {item.type === "image" && (
-                      <img src={item.content} alt="content" />
-                    )}
-                    {item.type === "gif" && (
-                      <img
-                        src={item.content}
-                        alt="content"
-                        style={{ maxWidth: "50%", maxHeight: "40%" }}
-                      />
-                    )}
-                  </div>
-                )}
+          {/* Editor message pages */}
+          {messages.length > 0 ? (
+            messages.map((item: any, index: number) => (
+              <Page key={index} number={item?.slideIndex ?? index}>
+                <div
+                  style={{
+                    position: "absolute",
+                    left: item?.x ?? 0,
+                    top: item?.y ?? 0,
+                  }}
+                >
+                  {item?.type === "text" && (
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: item?.content || "",
+                      }}
+                    />
+                  )}
+
+                  {item?.type === "image" && item?.content && (
+                    <img src={item.content} alt="content" />
+                  )}
+
+                  {item?.type === "gif" && item?.content && (
+                    <img
+                      src={item.content}
+                      alt="content"
+                      style={{ maxWidth: "50%", maxHeight: "40%" }}
+                    />
+                  )}
+                </div>
               </Page>
-            )
+            ))
+          ) : (
+            <Page>
+              <div style={{ padding: 30, textAlign: "center" }}>
+                <h3>No content available</h3>
+              </div>
+            </Page>
           )}
 
-          <PageCover className="flex items-center justify-center w-full mx-auto">
+          {/* Back cover */}
+          <PageCover>
             <Image
               src={"/newimage/logoGroup.png"}
               alt="logo"
               width={150}
               height={50}
-              className="mx-auto flex items-center justify-center"
             />
             <p className="bottom-text">
               Create a card like this one. Go to Groupwish.com
